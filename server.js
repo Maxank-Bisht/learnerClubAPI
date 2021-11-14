457e1611891e40a142789fdde47dcfd0656dadd3
Mayank Bisht <bishtmayank799@gmail.com>
2021-11-14 18:59:14 +0530

    init morgan middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 // Route files
 const bootcamp = require('./routes/bootcamps');
@@ -8,6 +9,11 @@ const bootcamp = require('./routes/bootcamps');
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Dev Logger middleware
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamp);
